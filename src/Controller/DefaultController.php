02512819a3d25f5369d26dd6e1e985f29086378c
arnaudbit <arnaud.bit@gmail.com>
2021-10-19 15:16:07 +0200

    Initial commit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/",name="home")
+     */
+    public function home(): Response {
+        return $this->render('/pages/games.html.twig');
+    }
+}
